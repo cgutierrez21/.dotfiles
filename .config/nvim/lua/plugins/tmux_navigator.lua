bbbe8790d2fda5e6424d785098584f38ff8da938
Cristian <41880357+cgutierrez21@users.noreply.github.com>
2024-01-22 14:03:27 -0600

    Update 2024-01-22 tmux navigator changes

--- .config/nvim/lua/plugins/tmux_navigator.lua
+++ .config/nvim/lua/plugins/tmux_navigator.lua
@@ -3,10 +3,6 @@ return {
 	config = function()
 		local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-		nvim_tmux_nav.setup({
-			disable_when_zoomed = true, -- defaults to false
-		})
-
 		vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<CR>")
 		vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<CR>")
 		vim.keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<CR>")
