b1d03b8539da771086b899fb8dc0d6cf160d1490
Cristian <41880357+cgutierrez21@users.noreply.github.com>
2024-03-10 17:49:10 -0500

    Update 2024-03-10 Initial neovim config cleanup

--- .config/nvim/lua/plugins/enhancements.lua
+++ .config/nvim/lua/plugins/enhancements.lua
@@ -2,37 +2,33 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "night-owl",
-					sections = {
-						lualine_a = { "mode" },
-						lualine_b = { "branch", "diff", "diagnostics" },
-						lualine_c = { "filename", "filetype" },
-						lualine_x = {},
-						lualine_y = { "encoding", "fileformat" },
-						lualine_z = { "progress" },
-					},
+		opts = {
+			options = {
+				theme = "night-owl",
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename", "filetype" },
+					lualine_x = {},
+					lualine_y = { "encoding", "fileformat" },
+					lualine_z = { "progress" },
 				},
-			})
-		end,
+			},
+		},
 	},
 
 	{
 		"folke/todo-comments.nvim",
 		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("todo-comments").setup({
-				keywords = {
-					DONE = { icon = " ", color = "done", alt = { "DONE" } },
-				},
-				colors = {
-					done = { "#00FF00" },
-				},
-			})
-		end,
+		opts = {
+			keywords = {
+				DONE = { icon = " ", color = "done", alt = { "DONE" } },
+			},
+			colors = {
+				done = { "#00FF00" },
+			},
+		},
 	},
 
 	{
@@ -64,15 +60,6 @@ return {
 				":TodoTrouble keywords=TODO,FIX,WARN<CR>",
 				{ desc = "Find TODOs, FIXes, WARNs" }
 			)
-		end,
-	},
-
-	{
-		"echasnovski/mini.pairs",
-		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-		version = "*",
-		config = function()
-			require("mini.pairs").setup()
 		end,
 	},
 
